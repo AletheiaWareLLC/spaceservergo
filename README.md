f7128b29a3afb250450774736d78b81ffc707fb4
Stuart Scott <stuart.murray.scott@gmail.com>
2018-11-30 15:56:34 -0800

    Use raw TCP for block, head, status, and write APIs
    - Persist block to file
    - Persist channel head to file
    - Add channel cache
    - Drop log.Fatal for log.Println
    - Serve head by channel name
    - Serve block by channel name and either block hash or message hash

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 spaceservergo
 =============
 
-This is a Go implementation of a Space Server - an end-to-end encrypted blockchain-based storage platform.
+This is a Go implementation of a Space Server - an end-to-end encrypted, blockchain storage platform.
 
 Build
 =====
 
     go build
+
+Install
+=======
+
+To allow binding to ports 80 and 443
+
+    sudo setcap CAP_NET_BIND_SERVICE=+eip ./spaceservergo
--- server.go
+++ server.go
@@ -17,9 +17,11 @@
 package main
 
 import (
+	"bytes"
 	"crypto/rsa"
-	//"encoding/base64"
-	//"fmt"
+	"encoding/base64"
+	"errors"
+	"fmt"
 	bc "github.com/AletheiaWareLLC/bcgo"
 	bcutils "github.com/AletheiaWareLLC/bcgo/utils"
 	space "github.com/AletheiaWareLLC/spacego"
@@ -28,70 +30,106 @@ import (
 	customer "github.com/stripe/stripe-go/customer"
 	subscription "github.com/stripe/stripe-go/sub"
 	"html/template"
-	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
 	"os"
+	"os/user"
 	"path"
+	"strings"
 	"time"
 )
 
-type HTMLHandler struct {
-}
-
-type BlockHandler struct {
-}
-
-type HeadHandler struct {
-}
-
-type WriteHandler struct {
-}
-
+var Cache string
 var Node *bc.Node
-var registrationChannel *bc.Channel
+var Channels map[string]*bc.Channel
 
 func main() {
+	// Load cache directory
+	c, ok := os.LookupEnv("CACHE")
+	if !ok {
+		u, err := user.Current()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		c = path.Join(u.HomeDir, "bc")
+	}
+	Cache = c
 	// Load private key
-	Key, err := space.GetOrCreatePrivateKey()
+	k, err := space.GetOrCreatePrivateKey()
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
 	Node = &bc.Node{
-		Key: Key,
+		Key: k,
 	}
-	registrationChannel = &bc.Channel{
-		Name:      "Space Registration",
+	registration := &bc.Channel{
+		Name:      space.SPACE_REGISTRATION,
 		Threshold: bc.THRESHOLD_STANDARD,
+		Cache:     Cache,
 	}
+	registration.LoadHead()
+	Channels = make(map[string]*bc.Channel)
+	Channels[space.SPACE_REGISTRATION] = registration
 
 	// Serve Block Requests
-	go func() {
-		log.Fatal(http.ListenAndServe(":23032", &BlockHandler{}))
-	}()
+	go bind(bc.PORT_BLOCK, handleBlock)
 	// Serve Head Requests
-	go func() {
-		log.Fatal(http.ListenAndServe(":23132", &HeadHandler{}))
-	}()
+	go bind(bc.PORT_HEAD, handleHead)
+	// Serve Status Requests
+	go bind(bc.PORT_STATUS, handleStatus)
 	// Serve Write Requests
-	go func() {
-		log.Fatal(http.ListenAndServe(":23232", &WriteHandler{}))
-	}()
-	// Serve HTML Requests
-	log.Fatal(http.ListenAndServe(":8080", &HTMLHandler{}))
-	// Serve HTML Requests over HTTPS
-	//log.Fatal(http.ListenAndServeTLS(":443", "server.crt", "server.key", &HTMLHandler{}))
+	go bind(bc.PORT_WRITE, handleWrite)
+	/*
+		// Serve HTTPS HTML Requests
+		go log.Fatal(http.ListenAndServeTLS(":443", "server.crt", "server.key", http.HandlerFunc(serve)))
+		// Redirect HTTP HTML Requests to HTTPS
+		log.Fatal(http.ListenAndServe(":80", http.HandlerFunc(redirect)))
+	*/
+	// Serve HTTP HTML Requests
+	log.Fatal(http.ListenAndServe(":80", http.HandlerFunc(serve)))
 }
 
-func (h *HTMLHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	log.Println("URL Path ", r.URL.Path)
+func bind(port int, handler func(net.Conn)) {
+	address := fmt.Sprintf(":%d", port)
+	l, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Println("Error listening", err)
+		return
+	}
+	defer l.Close()
+	log.Println("Listening on" + address)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Println("Error accepting", err)
+			return
+		}
+		go handler(conn)
+	}
+}
+
+func redirect(w http.ResponseWriter, r *http.Request) {
+	target := "https://" + r.Host + r.URL.Path
+	if len(r.URL.RawQuery) > 0 {
+		target += "?" + r.URL.RawQuery
+	}
+	log.Println("Redirecting to", target)
+	http.Redirect(w, r, target, http.StatusTemporaryRedirect)
+}
+
+func serve(w http.ResponseWriter, r *http.Request) {
+	log.Println("URL Path", r.URL.Path)
 	switch r.URL.Path {
 	case "/register":
 		switch r.Method {
 		case "GET":
 			t, err := template.ParseFiles("template/register.html")
 			if err != nil {
-				log.Fatal(err)
+				log.Println(err)
+				return
 			}
 			data := struct {
 				Description string
@@ -102,15 +140,20 @@ func (h *HTMLHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 				Key:         os.Getenv("STRIPE_PUBLISHABLE_KEY"),
 				Name:        "Space",
 			}
-			log.Println("Data ", data)
+			log.Println("Data", data)
 			err = t.Execute(w, data)
 			if err != nil {
-				log.Fatal(err)
+				log.Println(err)
+				return
 			}
 		case "POST":
-			log.Println("Request ", r)
+			log.Println("Request", r)
+			publicKeyFormat := r.PostFormValue("publicKeyFormat")
+			log.Println("PublicKeyFormat", publicKeyFormat)
 			publicKeyString := r.PostFormValue("publicKey")
+			log.Println("PublicKey", publicKeyString)
 			stripeEmail := r.PostFormValue("stripeEmail")
+			log.Println("Email", stripeEmail)
 			// stripeBillingName := r.PostFormValue("stripeBillingName")
 			// stripeBillingAddressLine1 := r.PostFormValue("stripeBillingAddressLine1")
 			// stripeBillingAddressCity := r.PostFormValue("stripeBillingAddressCity")
@@ -119,6 +162,7 @@ func (h *HTMLHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			// stripeBillingAddressCountryCode := r.PostFormValue("stripeBillingAddressCountryCode")
 			// stripeBillingAddressState := r.PostFormValue("stripeBillingAddressState")
 			stripeToken := r.PostFormValue("stripeToken")
+			log.Println("Token", stripeToken)
 			// stripeTokenType := r.PostFormValue("stripeTokenType")
 
 			stripe.Key = os.Getenv("STRIPE_SECRET_KEY")
@@ -129,11 +173,13 @@ func (h *HTMLHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 				Email:       stripe.String(stripeEmail),
 			}
 			if err := customerParams.SetSource(stripeToken); err != nil {
-				log.Fatal(err)
+				log.Println(err)
+				return
 			}
 			cus, err := customer.New(customerParams)
 			if err != nil {
-				log.Fatal(err)
+				log.Println(err)
+				return
 			}
 
 			productId := os.Getenv("STRIPE_PRODUCT_ID")
@@ -150,10 +196,15 @@ func (h *HTMLHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			}
 			sub, err := subscription.New(subscriptionParams)
 			if err != nil {
-				log.Fatal(err)
+				log.Println(err)
+				return
 			}
 
-			publicKeyBytes := []byte(publicKeyString)
+			publicKeyBytes, err := base64.RawURLEncoding.DecodeString(publicKeyString)
+			if err != nil {
+				log.Println(err)
+				return
+			}
 			publicKeyHash := bcutils.Hash(publicKeyBytes)
 
 			// Create registration
@@ -165,32 +216,37 @@ func (h *HTMLHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 				PlanId:         planId,
 				SubscriptionId: sub.ID,
 			}
-			log.Println("Registration: ", registration)
+			log.Println("Registration", registration)
 			data, err := proto.Marshal(registration)
 			if err != nil {
-				log.Fatal(err)
+				log.Println(err)
+				return
 			}
 
 			// Decode public key string to rsa.PublicKey
 			pub, err := bcutils.RSAPublicKeyFromBytes(publicKeyBytes)
 			if err != nil {
-				log.Fatal(err)
+				log.Println(err)
+				return
 			}
 
 			// Create list of recipients (user + server)
 			recipients := [2]*rsa.PublicKey{pub, &Node.Key.PublicKey}
+			log.Println("Recipients", recipients)
 
 			// Create message from server to recipients
 			message, err := bc.CreateMessage(Node.Key, recipients[:], nil, data)
 			if err != nil {
-				log.Fatal(err)
+				log.Println(err)
+				return
 			}
 
-			log.Println("Message: ", message)
+			log.Println("Message", message)
 
 			data, err = proto.Marshal(message)
 			if err != nil {
-				log.Fatal(err)
+				log.Println(err)
+				return
 			}
 
 			entries := [1]*bc.BlockEntry{
@@ -201,59 +257,148 @@ func (h *HTMLHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			}
 
 			// Mine message into blockchain
-			hash, block, err := Node.Mine(registrationChannel, entries[:])
+			hash, block, err := Node.Mine(Channels[space.SPACE_REGISTRATION], entries[:])
 			if err != nil {
-				log.Fatal(err)
+				log.Println(err)
+				return
 			}
-			log.Println("Hash: ", hash)
-			log.Println("Block: ", block)
+			log.Println("Hash", hash)
+			log.Println("Block", block)
 
 			http.Redirect(w, r, "/success.html", http.StatusFound)
 		default:
-			log.Println("Unsupported method: ", r.Method)
+			log.Println("Unsupported method", r.Method)
 		}
 	default:
 		http.ServeFile(w, r, path.Join("static", r.URL.Path))
 	}
 }
 
-func (h *BlockHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	log.Println("Block Request Handler not yet implemented ", r)
-}
-
-func (h *HeadHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	log.Println("Head Request Handler not yet implemented ", r)
-}
-
-func (h *WriteHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	log.Println("ServeHTTP:23232")
-	data, err := ioutil.ReadAll(r.Body)
+func handleBlock(conn net.Conn) {
+	defer conn.Close()
+	log.Println("handleBlock:22222")
+	request, err := bc.ReadReference(conn)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
-
-	// Unmarshal as StorageRequest
-	request := space.StorageRequest{}
-	if err = proto.Unmarshal(data, &request); err != nil {
-		log.Fatal(err)
+	log.Println("Reference", request)
+	channel := request.ChannelName
+	c, err := GetChannel(channel)
+	if err != nil {
+		log.Println(err)
+		return
 	}
-	log.Println("StorageRequest: ", request)
-	// TODO if storage request has customer id, bill customer
-	// TODO if storage request has payment token, bill token
-	// TODO if storage request has neither, payment error
+	hash := request.BlockHash
+	if hash != nil && len(hash) > 0 {
+		// Read from cache
+		block, err := bc.ReadBlockFile(c.Cache, hash)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		// Write to connection
+		if err := bc.WriteBlock(conn, block); err != nil {
+			log.Println(err)
+			return
+		}
+	} else {
+		hash := request.MessageHash
+		if hash != nil && len(hash) > 0 {
+			// Search through chain until message hash is found, and return the containing block
+			b := c.Head
+			for b != nil {
+				for _, e := range b.Entry {
+					if bytes.Equal(e.MessageHash, hash) {
+						log.Println("Found message, writing block")
+						// Write to connection
+						if err := bc.WriteBlock(conn, b); err != nil {
+							log.Println(err)
+						}
+						return
+					}
+				}
+				h := b.Previous
+				if h != nil && len(h) > 0 {
+					b, err = bc.ReadBlockFile(c.Cache, h)
+					if err != nil {
+						log.Println(err)
+						return
+					}
+				} else {
+					b = nil
+				}
+			}
+		} else {
+			log.Println("Missing block hash and message hash")
+			return
+		}
+	}
+}
 
-	// TODO if the user wants to register, POST to :8080/register with publicKey, and stripe info.
-	// If response is success, await new head of Space Registration channel, and decrypt to get customer id.
+func handleHead(conn net.Conn) {
+	defer conn.Close()
+	log.Println("handleHead:22232")
+	request, err := bc.ReadReference(conn)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println("Reference", request)
+	channel := request.ChannelName
+	c, err := GetChannel(channel)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	reference, err := bc.ReadHeadFile(c.Cache, channel)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	if err := bc.WriteReference(conn, reference); err != nil {
+		log.Println(err)
+		return
+	}
+}
 
-	// TODO check public key matches registered customer
+func handleStatus(conn net.Conn) {
+	defer conn.Close()
+	log.Println("handleStatus:23222")
+	log.Println("Status Request Handler not yet implemented", conn)
+}
+
+func handleWrite(conn net.Conn) {
+	defer conn.Close()
+	log.Println("handleWrite:23232")
+	request, err := space.ReadStorageRequest(conn)
+	if err != nil {
+		log.Println(err)
+		return
+	}
 
 	// Decode public key string to rsa.PublicKey
 	publicKey, err := bcutils.RSAPublicKeyFromBytes(request.PublicKey)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
 	publicKeyHash := bcutils.Hash(request.PublicKey)
-	publicKeyHashString := string(publicKeyHash)
+	publicKeyHashString := base64.RawURLEncoding.EncodeToString(publicKeyHash)
+	log.Println("StorageRequest", proto.Size(request), publicKeyHashString, request.CustomerId, request.PaymentId)
+	if len(request.CustomerId) > 0 {
+		// TODO check public key matches registered customer
+		// TODO bill customer
+	} else {
+		if len(request.PaymentId) > 0 {
+			// TODO bill payment token
+		} else {
+			log.Println("Missing payment information")
+			// TODO if the user wants to register, POST to :443/register with publicKey, and stripe info.
+			// If response is success, await new head of Space Registration channel, and decrypt to get customer id.
+			return
+		}
+	}
 
 	response := &space.StorageResponse{}
 
@@ -261,13 +406,16 @@ func (h *WriteHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	references := make([]*bc.Reference, 0, 2)
 
 	fileChannel := &bc.Channel{
-		Name:      "Space File " + publicKeyHashString,
+		Name:      space.SPACE_FILE_PREFIX + publicKeyHashString,
 		Threshold: bc.THRESHOLD_STANDARD,
-		Head:      nil, // TODO load from storage
+		Cache:     Cache,
 	}
+	fileChannel.LoadHead()
+	Channels[fileChannel.Name] = fileChannel
 	fileReference, err := Mine(fileChannel, publicKey, request.File, nil)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
 	// Add fileReference to response
 	response.File = fileReference
@@ -276,13 +424,16 @@ func (h *WriteHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	if request.Preview != nil {
 		previewChannel := &bc.Channel{
-			Name:      "Space Preview " + publicKeyHashString,
+			Name:      space.SPACE_PREVIEW_PREFIX + publicKeyHashString,
 			Threshold: bc.THRESHOLD_STANDARD,
-			Head:      nil, // TODO load from storage
+			Cache:     Cache,
 		}
+		previewChannel.LoadHead()
+		Channels[previewChannel.Name] = previewChannel
 		previewReference, err := Mine(previewChannel, publicKey, request.Preview, nil)
 		if err != nil {
-			log.Fatal(err)
+			log.Println(err)
+			return
 		}
 		// Add previewReference to response
 		response.Preview = previewReference
@@ -291,23 +442,25 @@ func (h *WriteHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 
 	metaChannel := &bc.Channel{
-		Name:      "Space Meta " + publicKeyHashString,
+		Name:      space.SPACE_META_PREFIX + publicKeyHashString,
 		Threshold: bc.THRESHOLD_STANDARD,
-		Head:      nil, // TODO load from storage
+		Cache:     Cache,
 	}
+	metaChannel.LoadHead()
+	Channels[metaChannel.Name] = metaChannel
 	metaReference, err := Mine(metaChannel, publicKey, request.Meta, references)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
 	response.Meta = metaReference
 
 	// Reply with storage response
-	log.Println("StorageResponse: ", response)
-	result, err := proto.Marshal(response)
-	if err != nil {
-		log.Fatal(err)
+	log.Println("StorageResponse", response)
+	if err := space.WriteStorageResponse(conn, response); err != nil {
+		log.Println(err)
+		return
 	}
-	w.Write(result)
 }
 
 func Mine(channel *bc.Channel, publicKey *rsa.PublicKey, bundle *space.StorageRequest_Bundle, references []*bc.Reference) (*bc.Reference, error) {
@@ -359,7 +512,8 @@ func Mine(channel *bc.Channel, publicKey *rsa.PublicKey, bundle *space.StorageRe
 	go func() {
 		_, _, err := Node.Mine(channel, entries[:])
 		if err != nil {
-			log.Fatal(err)
+			log.Println(err)
+			return
 		}
 	}()
 
@@ -369,4 +523,21 @@ func Mine(channel *bc.Channel, publicKey *rsa.PublicKey, bundle *space.StorageRe
 		ChannelName: channel.Name,
 		MessageHash: hash,
 	}, nil
+}
+
+func GetChannel(name string) (*bc.Channel, error) {
+	if strings.HasPrefix(name, space.SPACE_PREFIX) {
+		channel := Channels[name]
+		if channel == nil {
+			channel = &bc.Channel{
+				Name:      name,
+				Threshold: bc.THRESHOLD_STANDARD,
+				Cache:     Cache,
+			}
+			channel.LoadHead()
+			Channels[name] = channel
+		}
+		return channel, nil
+	}
+	return nil, errors.New("Unknown channel: " + name)
 }
